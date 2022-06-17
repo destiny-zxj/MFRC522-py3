f2caecf76cd4a486ebbde3f7a693db1c768decbb
destiny_zxj <destiny_zxj@163.com>
2022-06-17 14:50:11 +0800

    dev

---
+++ demo/demo_read.py
@@ -0,0 +1,16 @@
+from time import sleep
+import RPi.GPIO as GPIO
+from mfrc522.SimpleMFRC522 import SimpleMFRC522
+
+
+reader = SimpleMFRC522()
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        cid, text = reader.read()
+        print("ID: %s\nText: %s" % (cid, text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
