67f1e6e81ee8f44c26225464bed8d3974d11a9d1
destiny_zxj <destiny_zxj@163.com>
2022-06-17 14:45:46 +0800

    dev

--- demo/demo_write.py
+++ demo/demo_write.py
@@ -6,11 +6,7 @@ from mfrc522.SimpleMFRC522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-    while True:
-        print("Hold a tag near the reader")
-        cid, text = reader.read()
-        print("ID: %s\nText: %s" % (cid, text))
-        sleep(5)
+    reader.write('hello')
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
